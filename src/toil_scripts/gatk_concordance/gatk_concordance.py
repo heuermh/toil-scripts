a0a20e5b05d60e365654487f8ce0323e01637b77
Michael L Heuer <heuermh@acm.org>
2016-03-15 15:18:21 -0500

    adding ref.fa.dict parameter

--- src/toil_scripts/gatk_concordance/gatk_concordance.py
+++ src/toil_scripts/gatk_concordance/gatk_concordance.py
@@ -9,49 +9,25 @@ from toil.job import Job
 
 from toil_scripts import download_from_s3_url
 from toil_scripts.batch_alignment.bwa_alignment import upload_to_s3, docker_call
+from toil_scripts.gatk_germline.germline import download_url, read_from_filestore_hc
 
 def build_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--reference', required=True, help="Reference Genome URL")
     parser.add_argument('-i', '--reference_index', required=True, help="Reference Genome index (.fai) URL")
+    parser.add_argument('-d', '--reference_dict', required=True, help="Reference Genome sequence dictionary (.dict) URL")
     parser.add_argument('-1', '--eval_vcf', required=True, help="VCF file URL to evaluate")
     parser.add_argument('-2', '--comp_vcf', required=True, help="VCF file URL to compare against")
     parser.add_argument('-o', '--output_dir', required=True, help="Output directory S3 URL")
     return parser
 
 
-# copied from germline.py
-def download_url_c(job, url, filename):
-    work_dir = job.fileStore.getLocalTempDir()
-    file_path = os.path.join(work_dir, filename)
-    if not os.path.exists(file_path):
-        if url.startswith('s3:'):
-            download_from_s3_url(file_path, url)
-        else:
-            try:
-                subprocess.check_call(['curl', '-fs', '--retry', '5', '--create-dir', url, '-o', file_path])
-            except subprocess.CalledProcessError as cpe:
-                raise RuntimeError(
-                    '\nNecessary file could not be acquired: %s. Got error "%s". Check input URL' % (url, cpe))
-            except OSError:
-                raise RuntimeError('Failed to find "curl". Install via "apt-get install curl"')
-    assert os.path.exists(file_path)
-    return job.fileStore.writeGlobalFile(file_path)
-
-
-# copied from germline.py
-def read_from_filestore_c(job, work_dir, ids, *filenames):
-    for filename in filenames:
-        if not os.path.exists(os.path.join(work_dir, filename)):
-            job.fileStore.readGlobalFile(ids[filename], os.path.join(work_dir, filename))
-
-
 def batch_start(job, input_args):
-    shared_files = ['ref.fa', 'ref.fa.fai', 'eval.vcf', 'comp.vcf']
+    shared_files = ['ref.fa', 'ref.fa.fai', 'ref.fa.dict', 'eval.vcf', 'comp.vcf']
     shared_ids = {}
     for file_name in shared_files:
         url = input_args[file_name]
-        shared_ids[file_name] = job.addChildJobFn(download_url_c, url, file_name).rv()
+        shared_ids[file_name] = job.addChildJobFn(download_url, url, file_name).rv()
     job.addFollowOnJobFn(concordance, shared_ids, input_args)
     
 
@@ -60,8 +36,8 @@ def concordance(job, shared_ids, input_args):
     Evaluate concordance between two VCFs with GATK.
     """
     work_dir = job.fileStore.getLocalTempDir()
-    input_files = ['ref.fa', 'ref.fa.fai', 'eval.vcf', 'comp.vcf']
-    read_from_filestore_c(job, work_dir, shared_ids, *input_files)
+    input_files = ['ref.fa', 'ref.fa.fai', 'ref.fa.dict', 'eval.vcf', 'comp.vcf']
+    read_from_filestore_hc(job, work_dir, shared_ids, *input_files)
 
     eval_name = input_args['eval.vcf'].split('/')[-1]
     comp_name = input_args['comp.vcf'].split('/')[-1]
@@ -87,6 +63,7 @@ if __name__ == '__main__':
 
     inputs = {'ref.fa': args.reference,
               'ref.fa.fai': args.reference_index,
+              'ref.fa.dict': args.reference_dict,
               'eval.vcf': args.eval_vcf,
               'comp.vcf': args.comp_vcf,
               's3_dir': args.output_dir,
